70bde1717d66666411c368eeae56536c33c8e39e
Néstor Benitez <nestormbg1011@gmail.com>
2023-10-19 01:20:37 -0300

    Initial Commit

---
+++ billcollector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  billcollector
+//
+//  Created by Néstor Miguel Benitez Garay on 2023-10-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
